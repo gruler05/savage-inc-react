25da2e109e1eb45aa8ca916b180daa494c494500
gbansode <bansodegaurav@gmail.com>
2018-09-23 14:34:13 -0500

    Adding linting and other stuff

---
+++ src/components/loginForm/loginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+      </form>
+    );
+  }
+}
+
+export default LoginForm;
